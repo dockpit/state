bbd14965b136ae6566b851cb6c33ce967b4f39b6
Ad van der Veer <advanderveer@gmail.com>
2014-11-23 15:18:27 +0100

    first implementation of build

--- manager.go
+++ manager.go
@@ -1,7 +1,148 @@
 package state
 
-type Manager struct{}
+import (
+	"archive/tar"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"path/filepath"
 
-func NewManager() *Manager {
-	return &Manager{}
+	"github.com/fsouza/go-dockerclient"
+)
+
+type Manager struct {
+	Dir    string
+	Client *docker.Client
+}
+
+// Manages state for microservice testing by creating
+// docker images and starting containers when necessary
+func NewManager(host, cert, path string) (*Manager, error) {
+	m := &Manager{
+		Dir: path,
+	}
+
+	//parse docker host addr as url
+	hurl, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+
+	//change to http connection
+	hurl.Scheme = "https"
+
+	//create docker client
+	m.Client, err = docker.NewTLSClient(hurl.String(), filepath.Join(cert, "cert.pem"), filepath.Join(cert, "key.pem"), filepath.Join(cert, "ca.pem"))
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *Manager) ContainerName(pname, spath string) (string, error) {
+	hash := md5.New()
+	_, err := hash.Write([]byte(spath))
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("pitstate_%s_%s", pname, hex.EncodeToString(hash.Sum(nil))), nil
+}
+
+// Build a state by buildign Docker images
+func (m *Manager) Build(pname, sname string, out io.Writer) (string, error) {
+
+	//create writers
+	in := bytes.NewBuffer(nil)
+	tw := tar.NewWriter(in)
+
+	//expected context path for the state
+	root := filepath.Join(m.Dir, pname, fmt.Sprintf("'%s'", sname))
+
+	//walk and tar all files in a dir
+	//@from http://stackoverflow.com/questions/13611100/how-to-write-a-directory-not-just-the-files-in-it-to-a-tar-gz-file-in-golang
+	visit := func(fpath string, fi os.FileInfo, err error) error {
+
+		//cancel walk if something went wrong
+		if err != nil {
+			return err
+		}
+
+		//skip root
+		if fpath == root {
+			return nil
+		}
+
+		//dont 'add' dirs to archive
+		if fi.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(fpath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		//use relative path inside archive
+		rel, err := filepath.Rel(root, fpath)
+		if err != nil {
+			return err
+		}
+
+		//create header from file info struct
+		hdr, err := tar.FileInfoHeader(fi, rel)
+		if err != nil {
+			return err
+		}
+
+		//write header to archive
+		// hdr.Name = rel?
+		err = tw.WriteHeader(hdr)
+		if err != nil {
+			return err
+		}
+
+		//copy content into archive
+		if _, err = io.Copy(tw, f); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	//walk the context and create archive
+	if err := filepath.Walk(root, visit); err != nil {
+		return "", err
+	}
+
+	cname, err := m.ContainerName(pname, root)
+	if err != nil {
+		return "", err
+	}
+
+	//name after provider and hash of state name
+	bopts := docker.BuildImageOptions{
+		Name:         cname,
+		InputStream:  in,
+		OutputStream: out,
+	}
+
+	//issue build command to docker host
+	if err := m.Client.BuildImage(bopts); err != nil {
+		return "", err
+	}
+
+	return cname, nil
+}
+
+// Start a state by running a Docker container from an image
+func (m *Manager) Start(pname, sname string) error {
+
+	return fmt.Errorf("not implemented")
 }
--- manager_test.go
+++ manager_test.go
@@ -1,16 +1,56 @@
 package state_test
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
 	"testing"
 
-	// "github.com/bmizerany/assert"
+	"github.com/bmizerany/assert"
 
 	"github.com/dockpit/state"
 )
 
+func getmanager(t *testing.T) *state.Manager {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := os.Getenv("DOCKER_HOST")
+	if h == "" {
+		t.Skip("No DOCKER_HOST env variable setup")
+	}
+
+	cert := os.Getenv("DOCKER_CERT_PATH")
+	if cert == "" {
+		t.Skip("No DOCKER_CERT_PATH env variable setup")
+	}
+
+	m, err := state.NewManager(h, cert, filepath.Join(wd, "docs", "states"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return m
+}
+
 func TestBuild(t *testing.T) {
+	m := getmanager(t)
+	out := bytes.NewBuffer(nil)
 
-	m := state.NewManager()
+	iname, err := m.Build("mysql", "a single user", out)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	_ = m
+	match, err := regexp.MatchString(`(?s).*Successfully built.*`, out.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, `pitstate_mysql_c189303ee6bcedc685646c70a493ed27`, iname)
+	assert.NotEqual(t, false, match, fmt.Sprintf("unexpected build output: %s", out.String()))
 }
